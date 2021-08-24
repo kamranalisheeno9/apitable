97501f927a28da1632d7c008cf4d6d1ba5145fe9
unknown <kamranalisheeno9@gmail.com>
2021-08-24 10:07:52 +0500

    modification

--- src/pages/page1.js
+++ src/pages/page1.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DataTable1 from './page1data.json'
 import './page1.css'
-import { Container, Table,Button } from 'react-bootstrap';
+import { Container, Table, Button } from 'react-bootstrap';
 import {
     BrowserRouter as Router,
     Switch,
@@ -10,111 +10,111 @@ import {
     Link,
     useRouteMatch,
     useParams
-  } from "react-router-dom";
+} from "react-router-dom";
 const Table1 = () => {
-    
+
     return (
         <>
-        <Container className="table-1">
-            <h1>"Environnement de RECETTE"</h1><br />
-            <Table bordered >
-                <thead>
-                    <tr className="double-header" >
-                        <th className="Column">Etapes</th>
-                        <th className="Column">Descriptif</th>
-                        <th>PROD <br /> 
-                        <th className="E">E0ED</th>
-                        </th>
-                        <th>PRE PROD
+            <Container className="table-1">
+                <h1>"Environnement de RECETTE"</h1><br />
+                <Table bordered >
+                    <thead>
+                        <tr className="double-header" >
+                            <th className="Column">Etapes</th>
+                            <th className="Column">Descriptif</th>
+                            <th>PROD <br />
+                                <th className="E">E0ED</th>
+                            </th>
+                            <th>PRE PROD
 
-                        <th className="E">E1ED</th>
+                                <th className="E">E1ED</th>
 
-                        </th>
-                        <th>RECETTE
-                        <th className="E">E2ED</th>
-                            
-                             </th>
-                        <th colSpan="2">RECETTE NEO
-                        <th className="E">EHED</th>
-                        <th className="inner-td">ENED</th>
-                        
-                        </th>
-                        <th colSpan="2">RECETTE ONE
-                        <th className="E">ERED  </th>
-                        <th className="inner-td">ELED</th>
-                        
-                        </th>
-                    </tr>
+                            </th>
+                            <th>RECETTE
+                                <th className="E">E2ED</th>
+
+                            </th>
+                            <th colSpan="2">RECETTE NEO
+                                <th className="E">EHED</th>
+                                <th className="inner-td">ENED</th>
+
+                            </th>
+                            <th colSpan="2">RECETTE ONE
+                                <th className="E">ERED  </th>
+                                <th className="inner-td">ELED</th>
+
+                            </th>
+                        </tr>
 
 
-                </thead>
-                <tbody className="table-1-body">
-                    <tr>
-                    <td>Distribution</td>
-                        <td>Envoi Des fichiers vers le faconnier</td>
-                       {DataTable1.map((data,index)=>{
-                           return(
-                            <>
-                                    <td key={index} className="green" >{data.ok1}</td>
-                            
-                           </>
-                           )
-                       })}
+                    </thead>
+                    <tbody className="table-1-body">
+                        <tr>
+                            <td>Distribution</td>
+                            <td>Envoi Des fichiers vers le faconnier</td>
+                            {DataTable1.map((data, index) => {
+                                return (
+                                    <>
+                                        <td key={index} className="green" >{data.ok1}</td>
 
-                    </tr>
-                    <tr >
-                        <td>Traitements</td>
-                        <td>Demon de suivi</td>
-                        {DataTable1.map((data,index)=>{
-                           return(
-                            <>
-                                    <td key={index} className="green" >{data.ok2}</td>
-                            
-                           </>
-                           )
-                       })}
-                        
+                                    </>
+                                )
+                            })}
 
-                    </tr>
-                    <tr >
-                        <td>Demon de suivi</td>
-                        <td>Routine des mise a jour de la station em temps reel</td>
-                        {DataTable1.map((data,index)=>{
-                           return(
-                            <>
-                                    <td key={index} className="green" >{data.ok3}</td>
-                            
-                           </>
-                           )
-                       })}
-                        
-                    </tr>
-                    <tr >
-                        <td>Station + WS</td>
-                        <td>Acces a la station et aux web services</td>
-                        {DataTable1.map((data,index)=>{
-                           return(
-                            <>
-                                    <td key={index} className="green" >{data.ok4}</td>
-                            
-                           </>
-                           )
-                       })}
-                        
+                        </tr>
+                        <tr >
+                            <td>Traitements</td>
+                            <td>Demon de suivi</td>
+                            {DataTable1.map((data, index) => {
+                                return (
+                                    <>
+                                        <td key={index} className="green" >{data.ok2}</td>
 
-                    </tr>
-                </tbody>
-            </Table>
-        </Container>
+                                    </>
+                                )
+                            })}
+
+
+                        </tr>
+                        <tr >
+                            <td>Demon de suivi</td>
+                            <td>Routine des mise a jour de la station em temps reel</td>
+                            {DataTable1.map((data, index) => {
+                                return (
+                                    <>
+                                        <td key={index} className="green" >{data.ok3}</td>
+
+                                    </>
+                                )
+                            })}
+
+                        </tr>
+                        <tr >
+                            <td>Station + WS</td>
+                            <td>Acces a la station et aux web services</td>
+                            {DataTable1.map((data, index) => {
+                                return (
+                                    <>
+                                        <td key={index} className="green" >{data.ok4}</td>
+
+                                    </>
+                                )
+                            })}
+
+
+                        </tr>
+                    </tbody>
+                </Table>
+            </Container>
             <Container className="btn-container">
 
-<Link to="/page2">
-<Button className="btn" >
-    Next
-    </Button>
-    </Link>
-</Container>
-</>
+                <Link to="/page2">
+                    <Button className="btn" >
+                        Next
+                    </Button>
+                </Link>
+            </Container>
+        </>
     );
 }
 
--- src/pages/page2.js
+++ src/pages/page2.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './page2.css'
 import DataTable2 from './page2data.json'
-import { Container, Table,Button } from 'react-bootstrap';
+import { Container, Table, Button } from 'react-bootstrap';
 import {
     BrowserRouter as Router,
     Switch,
@@ -10,44 +10,44 @@ import {
     Link,
     useRouteMatch,
     useParams
-  } from "react-router-dom";
+} from "react-router-dom";
 const Table2 = () => {
     return (
         <>
-        <Container className="table-2">
-            <h1>"Environnement de RECETTE"</h1><br />
-            <Table  bordered hover>
-                <thead>
-                    <tr>
-                        <th>Repertoire/Fichier</th>
-                        <th>Etat</th>
-                        <th>Detail</th>
-                    </tr>
-                </thead>
-                <tbody className="table-2-body">
-                    {DataTable2.map(data => {
-                        return (
-                            <tr className={data.etat ? "green":"red"} key={data.id}>
-                                <td>{data.obj}</td>
-                                <td >{data.etat ? "Ok":"KO"}</td>
-                                <td>{data.detail}</td>
-                            </tr>
-                        )
-                    })}
+            <Container className="table-2">
+                <h1>"Environnement de RECETTE"</h1><br />
+                <Table bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Repertoire/Fichier</th>
+                            <th>Etat</th>
+                            <th>Detail</th>
+                        </tr>
+                    </thead>
+                    <tbody className="table-2-body">
+                        {DataTable2.map(data => {
+                            return (
+                                <tr className={data.etat ? "green" : "red"} key={data.id}>
+                                    <td>{data.obj}</td>
+                                    <td >{data.etat ? "Ok" : "KO"}</td>
+                                    <td>{data.detail}</td>
+                                </tr>
+                            )
+                        })}
 
 
-                </tbody>
-            </Table>
-        </Container>
-        <Container className="btn-container">
+                    </tbody>
+                </Table>
+            </Container>
+            <Container className="btn-container">
 
-                            <Link to="/">
-            <Button className="btn" >
-                                Previous
-                                </Button>
-                                </Link>
-        </Container>
-                                </>
+                <Link to="/">
+                    <Button className="btn" >
+                        Previous
+                    </Button>
+                </Link>
+            </Container>
+        </>
     );
 }
 
